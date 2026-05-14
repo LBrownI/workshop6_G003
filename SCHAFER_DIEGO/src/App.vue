e2a8f5fe7a529e3e7c4806aff0472d8805d2950a
DiegoSchafer24 <dschaferm1@correo.uss.cl>
2026-05-14 12:22:44 -0400

    Contenido evaluación

--- SCHAFER_DIEGO/src/App.vue
+++ SCHAFER_DIEGO/src/App.vue
@@ -1,7 +1,22 @@
+<template>
+  <h1 id="center">Dashboard  del estudiante</h1>
+  <div>
+    <p>Nombre del estudiante</p>
+    <UserProfile nombre="Diego Schafer" />
+    <p>Carrera</p>
+    <UserProfile carrera="Ingeniería Civíl Informática" />
+  </div>
+  <hr>
+  <div>
+    <p>Asignaturas aprobadas</p>
+    <UserStats aprobadas="40" />
+    <p>Promedio de notas</p>
+    <UserStats promedio="5.8" />
+  </div>
+</template>
+
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import UserProfile from './components/UserProfile.vue';
+import UserStats from './components/UserStats.vue';
 </script>
 
-<template>
-  <HelloWorld />
-</template>
