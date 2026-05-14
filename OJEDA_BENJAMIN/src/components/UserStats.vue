fd97988d027140de47352b03e15168b557b88c41
bwnjawn <benjaminojedaval@gmail.com>
2026-05-14 12:23:04 -0400

    Taller Benjamin Ojeda

---
+++ OJEDA_BENJAMIN/src/components/UserStats.vue
@@ -0,0 +1,34 @@
+<script >
+export default {
+	props: {
+		cantAsignaturas: {
+			type: Number,
+			required: true
+		},
+		promedioAcumulado: {
+			type: Number,
+			required: true
+		}
+	}
+};
+</script>
+<template>
+    <table border="1">
+    <caption>Estadisticas Academicas</caption>
+    <thead>
+        <tr>
+        <th>Asignaturas Aprobadas</th>
+        <th>PROMEDIO ACUMULADO </th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+        <td>{{cantAsignaturas}}</td>
+        </tr>
+        <tr>
+        <td>{{ promedioAcumulado }}</td>
+        </tr>
+    </tbody>
+    </table>
+
+</template>
