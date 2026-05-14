2fb20ccec3a8f1d064058b6716d6adbf220ca6ec
Alonso Cárdenas <alcardenascastroamador@gmail.com>
2026-05-14 12:19:10 -0400

    Initial Commit

---
+++ CARDENAS_ALONSO/src/components/UserStats.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+const props = defineProps(['aproved', 'promedio'])
+</script>
+
+<template>
+    <div class="columns">
+    <div class="column"> Asignaturas Aprovadas:
+    <div class="is-size-3"><b>{{ props.aproved }}</b></div></div>
+    <div class="column"> Promedio Acumulado: <div class="is-size-3"><b>{{ props.promedio }}</b></div></div>
+    </div>
+
+    <button class=" button is-primary"> Avanzar Semestre (+1 Asignatura) </button>
+</template>
+
+<style>
+</style>
